b51b4599568813262dcc3d3f41a3fa51e7c62727
Walter Tominaga <walter.tominaga@gmail.com>
2023-12-20 16:52:56 -0300

    Update mysql_log_vew.php
    set names utf8

--- mysql_log_vew.php
+++ mysql_log_vew.php
@@ -26,8 +26,7 @@
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // echo "Connected successfully";
-
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec("set names utf8");
 
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
